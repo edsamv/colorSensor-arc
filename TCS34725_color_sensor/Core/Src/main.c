7e4e269a840c5f81d046157d04ef015ced9cc928
Edwin Sam <edsamv29@gmail.com>
2026-03-26 20:00:13 +0800

    updating cubemx files

--- TCS34725_color_sensor/Core/Src/main.c
+++ TCS34725_color_sensor/Core/Src/main.c
@@ -1,26 +1,26 @@
-	/* USER CODE BEGIN Header */
+/* USER CODE BEGIN Header */
 	/**
 	  ******************************************************************************
 	  * @file           : main.c
 	  * @brief          : ARC 2026 - Direct Button Logic (No Timers) | LED PB8
 	  ******************************************************************************
 	  */
-	/* USER CODE END Header */
-	/* Includes ------------------------------------------------------------------*/
-	#include "main.h"
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
-	/* Private includes ----------------------------------------------------------*/
-	/* USER CODE BEGIN Includes */
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 	#include <math.h>
-	/* USER CODE END Includes */
+/* USER CODE END Includes */
 
-	/* Private typedef -----------------------------------------------------------*/
-	/* USER CODE BEGIN PTD */
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-	/* USER CODE END PTD */
+/* USER CODE END PTD */
 
-	/* Private define ------------------------------------------------------------*/
-	/* USER CODE BEGIN PD */
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
 	// I2C Address
 	#define TCS34725_ADDR        (0x29 << 1)
 
@@ -75,27 +75,27 @@
 	} debug_notnormalized;
 
 
-	/* USER CODE END PD */
+/* USER CODE END PD */
 
-	/* Private macro -------------------------------------------------------------*/
-	/* USER CODE BEGIN PM */
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
 
-	/* USER CODE END PM */
+/* USER CODE END PM */
 
-	/* Private variables ---------------------------------------------------------*/
-	I2C_HandleTypeDef hi2c1;
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
 
-	/* USER CODE BEGIN PV */
+/* USER CODE BEGIN PV */
 
 
 
-	/* USER CODE END PV */
+/* USER CODE END PV */
 
-	/* Private function prototypes -----------------------------------------------*/
-	void SystemClock_Config(void);
-	static void MX_GPIO_Init(void);
-	static void MX_I2C1_Init(void);
-	/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
 	float calculate_distance(float current[3], float target[3]);
 	void write8(uint8_t reg, uint8_t value);
 	uint16_t read16(uint8_t reg);
@@ -107,44 +107,44 @@
 	void blink_error();
 	void wait_all_buttons_release();
 	int get_layout_id(int arr[], int length);
-	/* USER CODE END PFP */
+/* USER CODE END PFP */
 
-	/* Private user code ---------------------------------------------------------*/
-	/* USER CODE BEGIN 0 */
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 
-	/* USER CODE END 0 */
+/* USER CODE END 0 */
 
-	/**
-	  * @brief  The application entry point.
-	  * @retval int
-	  */
-	int main(void)
-	{
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
 
-	  /* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-	  /* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	  /* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	  HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	  /* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	  /* USER CODE END Init */
+  /* USER CODE END Init */
 
-	  /* Configure the system clock */
-	  SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	  /* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	  /* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	  /* Initialize all configured peripherals */
-	  MX_GPIO_Init();
-	  MX_I2C1_Init();
-	  /* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
 	  write8(TCS34725_ATIME, 0xEB);
 	  write8(TCS34725_CONTROL, 0x01);
 
@@ -152,211 +152,207 @@
 	  HAL_Delay(3);
 	  write8(TCS34725_ENABLE, 0x03);
 	  HAL_Delay(100);
-	  /* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	  /* Infinite loop */
-	  /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	  while (1)
 	  {
-		/* USER CODE END WHILE */
-
+    /* USER CODE END WHILE */
 		  int b0 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0);
 		  int b1 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
-			  // CALIBRATION
-			  if (b0 && b1	) {
-				  HAL_Delay(2000);
+		  // CALIBRATION
+		  if (b0 && b1	) {
+			  HAL_Delay(2000);
 
-				  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) &&
+			  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) &&
 					  HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1)) {
 
-					  run_calibration();
-					  blink_success();
+				  run_calibration();
+				  blink_success();
+				  leds_off();
+
+				  wait_all_buttons_release();
+			  }
+		  }
+
+		  // STORE
+		  else if (b0) {
+
+			  HAL_Delay(50);  // debounce
+
+			  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0)) {
+
+				  if (index_ptr >= MAX_COLORS) {
+					  blink_error();
 					  leds_off();
-
 					  wait_all_buttons_release();
+					  continue;
 				  }
-			  }
 
-			  // STORE
-			  else if (b0) {
+				  int color = get_color_id();
+				  show_color(color);
+				  HAL_Delay(300);
 
-				  HAL_Delay(50);  // debounce
-
-				  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0)) {
-
-					  if (index_ptr >= MAX_COLORS) {
-						  blink_error();
-						  leds_off();
-						  wait_all_buttons_release();
-						  continue;
-					  }
-
-					  int color = get_color_id();
-					  show_color(color);
-					  HAL_Delay(300);
-
-					  if (color != COLOR_NONE) {
-						  colorArray[index_ptr++] = color;
-						  blink_success();
-					  }
-					  else {
-						  blink_error();
-					  }
-
-					  wait_all_buttons_release();
+				  if (color != COLOR_NONE) {
+					  colorArray[index_ptr++] = color;
+					  blink_success();
+		 					  }
+				  else {
+					  blink_error();
 				  }
+
+				  wait_all_buttons_release();
 			  }
+		  }
 
-			  // UNDO
-			  else if (b1) {
+		  // UNDO
+		  else if (b1) {
 
-				  HAL_Delay(50);  // debounce
+			  HAL_Delay(50);  // debounce
 
-				  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1)) {
+			  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1)) {
 
-					  if (index_ptr > 0) {
-						  index_ptr--;
+				  if (index_ptr > 0) {
+					  index_ptr--;
+					  blink_success();   // blink
+					  leds_off();        // then turn off
 
-						  blink_success();   // blink
-						  leds_off();        // then turn off
-
-					  } else {
-						  blink_error();
-						  leds_off();
-					  }
-
-					  wait_all_buttons_release();
+				  } else {
+					  blink_error();
+					  leds_off();
 				  }
+
+				  wait_all_buttons_release();
 			  }
+		  }
 
-			  HAL_Delay(50);
+		  HAL_Delay(50);
 
-			  if (index_ptr > 0) {
-			      layout_id = get_layout_id(colorArray, index_ptr);
-			  }
+		  if (index_ptr > 0) {
+			  layout_id = get_layout_id(colorArray, index_ptr);
+		  }		 			  }
+    /* USER CODE BEGIN 3 */
+  /* USER CODE END 3 */
+}
 
-		/* USER CODE BEGIN 3 */
-	  }
-	  /* USER CODE END 3 */
-	}
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/**
-	  * @brief System Clock Configuration
-	  * @retval None
-	  */
-	void SystemClock_Config(void)
-	{
-	  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-	  /** Configure the main internal regulator output voltage
-	  */
-	  __HAL_RCC_PWR_CLK_ENABLE();
-	  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV8;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	  /** Initializes the RCC Oscillators according to the specified parameters
-	  * in the RCC_OscInitTypeDef structure.
-	  */
-	  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	  RCC_OscInitStruct.PLL.PLLM = 8;
-	  RCC_OscInitStruct.PLL.PLLN = 168;
-	  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV8;
-	  RCC_OscInitStruct.PLL.PLLQ = 4;
-	  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	  {
-		Error_Handler();
-	  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	  /** Initializes the CPU, AHB and APB buses clocks
-	  */
-	  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-								  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
 
-	  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-	  {
-		Error_Handler();
-	  }
-	}
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
 
-	/**
-	  * @brief I2C1 Initialization Function
-	  * @param None
-	  * @retval None
-	  */
-	static void MX_I2C1_Init(void)
-	{
+  /* USER CODE BEGIN I2C1_Init 0 */
 
-	  /* USER CODE BEGIN I2C1_Init 0 */
+  /* USER CODE END I2C1_Init 0 */
 
-	  /* USER CODE END I2C1_Init 0 */
+  /* USER CODE BEGIN I2C1_Init 1 */
 
-	  /* USER CODE BEGIN I2C1_Init 1 */
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
 
-	  /* USER CODE END I2C1_Init 1 */
-	  hi2c1.Instance = I2C1;
-	  hi2c1.Init.ClockSpeed = 400000;
-	  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-	  hi2c1.Init.OwnAddress1 = 0;
-	  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-	  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-	  hi2c1.Init.OwnAddress2 = 0;
-	  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-	  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-	  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-	  {
-		Error_Handler();
-	  }
-	  /* USER CODE BEGIN I2C1_Init 2 */
+  /* USER CODE END I2C1_Init 2 */
 
-	  /* USER CODE END I2C1_Init 2 */
+}
 
-	}
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
 
-	/**
-	  * @brief GPIO Initialization Function
-	  * @param None
-	  * @retval None
-	  */
-	static void MX_GPIO_Init(void)
-	{
-	  GPIO_InitTypeDef GPIO_InitStruct = {0};
-	  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
-	  /* USER CODE END MX_GPIO_Init_1 */
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-	  /* GPIO Ports Clock Enable */
-	  __HAL_RCC_GPIOB_CLK_ENABLE();
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
-	  /*Configure GPIO pin Output Level */
-	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+  /*Configure GPIO pins : PB0 PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	  /*Configure GPIO pins : PB0 PB1 PB2 */
-	  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2;
-	  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	  GPIO_InitStruct.Pull = GPIO_NOPULL;
-	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /*Configure GPIO pins : PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	  /*Configure GPIO pins : PB8 PB9 */
-	  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
-	  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	  GPIO_InitStruct.Pull = GPIO_NOPULL;
-	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
 
-	  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
+}
 
-	  /* USER CODE END MX_GPIO_Init_2 */
-	}
-
-	/* USER CODE BEGIN 4 */
+/* USER CODE BEGIN 4 */
 
 	void wait_all_buttons_release() {
 		while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) ||
@@ -528,35 +524,35 @@
 
 		return (buf[1] << 8) | buf[0];
 	}
-	/* USER CODE END 4 */
+/* USER CODE END 4 */
 
-	/**
-	  * @brief  This function is executed in case of error occurrence.
-	  * @retval None
-	  */
-	void Error_Handler(void)
-	{
-	  /* USER CODE BEGIN Error_Handler_Debug */
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 	  /* User can add his own implementation to report the HAL error return state */
 	  __disable_irq();
 	  while (1)
 	  {
 	  }
-	  /* USER CODE END Error_Handler_Debug */
-	}
-	#ifdef USE_FULL_ASSERT
-	/**
-	  * @brief  Reports the name of the source file and the source line number
-	  *         where the assert_param error has occurred.
-	  * @param  file: pointer to the source file name
-	  * @param  line: assert_param error line source number
-	  * @retval None
-	  */
-	void assert_failed(uint8_t *file, uint32_t line)
-	{
-	  /* USER CODE BEGIN 6 */
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
 	  /* User can add his own implementation to report the file name and line number,
 		 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	  /* USER CODE END 6 */
-	}
-	#endif /* USE_FULL_ASSERT */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
